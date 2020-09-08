89278d72f1e98b2c5c7006dfe574fbb2fdb7ae7f
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-09-08 23:14:01 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+	//Fill the values to MySQL connection ($con)
+	$con_hostname = "localhost";
+	$con_user_name = "T1083170";
+	$con_pswd = "#infy123";
+	$con_db_name = "os_T1083170";
+	$con = mysqli_connect($con_hostname, $con_user_name, $con_pswd, $con_db_name);
+?>
