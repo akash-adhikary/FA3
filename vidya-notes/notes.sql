424a606259a2db047125a554a0f65691f2b2cd1f
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-09-09 02:10:08 +0530

    Create notes.sql

---
+++ vidya-notes/notes.sql
@@ -0,0 +1,11 @@
+use database os_T1082806;
+
+create table notes
+(
+email varchar(20),
+datecreated date,
+title varchar(50),
+notebody text
+);
+
+insert into table values("nuzha@gmail.com","2020-08-10","Akash","abcabacabac");
