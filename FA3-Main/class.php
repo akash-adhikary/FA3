c0fa88a2f9928d8630e4362b6d907c30cefd11d6
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-09-09 02:16:10 +0530

    Create class.php

---
+++ FA3-Main/class.php
@@ -0,0 +1,23 @@
+<?php 
+    class Admin
+	{
+			public grocery_name;
+			public quantity;
+			public price_per_unit;
+			
+			function _construct($grocery_name,$quantity,$price_per_unit)
+			{
+				$this->grocery_name=$grocery_name;
+				$this->quantity=$quantity;
+				$this->price_per_unit=$price_per_unit;
+			}
+			function validate_grocery_name()
+			{
+				
+			}
+			function add_grocery()
+			{
+				
+			}
+	}
+?>
