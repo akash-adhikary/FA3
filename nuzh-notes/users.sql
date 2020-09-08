ddea8463574c27d7580e719db0ac233d105bbec8
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2020-09-09 02:00:19 +0530

    Create users.sql

---
+++ nuzh-notes/users.sql
@@ -0,0 +1,10 @@
+drop table if exists nuz_note;
+create table nuz_note(
+    name varchar(20),
+    email varchar(50),
+    phone bigint(20),
+    username varchar(20),
+	password varchar(20),
+    age int(20));
+
+insert into nuz_note values("nuzha","nuzha.anam@gmail.com",6361123305,"nuzha.anam","Akash5@#",23);
